05ddee8ad4489d0658d1ae733efd22095e9c20cb
Tramaine Gillus <tramaine.gillus@gmail.com>
2021-11-25 12:35:22 -0500

    Improve layout of pack it test.

--- test/cli/pack-it.test.ts
+++ test/cli/pack-it.test.ts
@@ -1,8 +1,8 @@
-jest.mock('../../src/cli/program');
-jest.mock('../../src/utils/log');
-
 import '../../src/cli/pack-it';
 import { program } from '../../src/cli/program';
+
+jest.mock('../../src/cli/program');
+jest.mock('../../src/utils/log');
 
 test('executes program', () => {
   expect(program.parseAsync).toBeCalledTimes(1);
