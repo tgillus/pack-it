3d2781d0950e97e261e68ac21b312ee139b045bf
Tramaine Gillus <tramaine.gillus@gmail.com>
2022-07-22 23:20:20 -0400

    chore: add task list

---
+++ src/cli/ui/task-list.ts
@@ -0,0 +1,58 @@
+import delay from 'delay';
+import { Listr, ListrTask } from 'listr2';
+import { title } from './title.js';
+
+export class TaskList {
+  constructor(private readonly tasks: Task[]) {}
+
+  add(tasks: Task[]) {
+    this.tasks.push(...tasks);
+  }
+
+  async run() {
+    await new Listr(this.tasks.map((task) => task.prepare())).run();
+  }
+
+  static from(tasks: Task[]) {
+    return new TaskList(tasks);
+  }
+}
+
+export class Task {
+  constructor(private readonly item: TaskListItem) {}
+
+  prepare(): ListrTask {
+    const { action, title } = this.item;
+
+    return {
+      title,
+      task: async () => {
+        await action();
+      },
+    };
+  }
+}
+
+export interface TaskListItem {
+  readonly title: string;
+  readonly action: () => Promise<void>;
+}
+
+title();
+
+(async () => {
+  TaskList.from([
+    new Task({
+      title: 'foo',
+      action: async () => {
+        await delay(3000);
+      },
+    }),
+    new Task({
+      title: 'bar',
+      action: async () => {
+        await delay(3000);
+      },
+    }),
+  ]).run();
+})();
