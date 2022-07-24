7889ac8e7fc5f48ae8170c465b931ea8c994baa1
Tramaine Gillus <tramaine.gillus@gmail.com>
2022-07-24 14:09:58 -0400

    test: add fs tests

---
+++ test/utensils/file-system.test.ts
@@ -0,0 +1,40 @@
+import test from 'ava';
+import fs from 'fs';
+import * as td from 'testdouble';
+import { FileSystem } from '../../src/utensils/file-system/file-system.js';
+import { Operations } from '../../src/utensils/file-system/operations.js';
+import { Zip } from '../../src/utensils/file-system/zip.js';
+
+const operations = td.object<Operations>();
+const zip = td.object<Zip>();
+
+const fileSystem = new FileSystem(operations, zip);
+
+test.afterEach(() => {
+  td.reset();
+});
+
+test('creates directories', () => {
+  fileSystem.mkdir('foo');
+
+  td.verify(operations.mkdir('foo'));
+});
+
+test('remove files/directories', () => {
+  const patterns = ['foo', 'bar'];
+
+  fileSystem.rm(...patterns);
+
+  td.verify(operations.rm(patterns));
+});
+
+test('builds zip file', () => {
+  const patterns = ['foo', 'bar', 'baz'];
+  const destination = 'qux';
+  const stream = td.object<fs.WriteStream>();
+  td.when(operations.writeStream(destination)).thenReturn(stream);
+
+  fileSystem.zip(destination, ...patterns);
+
+  td.verify(zip.zip(stream, patterns));
+});
